16ed7de4a51f326adaf5e7692c7071420f3c0b1b
JuHeonHa <hjhjaueon97@gmail.com>
2024-11-14 13:13:58 +0900

    design: gnb에 z index

--- src/components/gnb.tsx
+++ src/components/gnb.tsx
@@ -3,7 +3,7 @@ import Navigator from "@/components/navigator";
 
 export default function GNB() {
   return (
-    <header className="flex fixed top-0 left-0 py-2 right-0 w-full bg-gray-100">
+    <header className="flex fixed top-0 left-0 py-2 right-0 w-full bg-gray-100 z-10">
       <div className="flex items-center justify-between w-full  mx-auto px-4 max-w-[1200px] m-auto">
         <Navigator />
         <SocialLogin />
