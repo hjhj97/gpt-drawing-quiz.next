e2b8deff2a2d763dd45a8a0dd8d8f1be9c2bd26e
JuHeonHa <hjhjaueon97@gmail.com>
2024-11-17 00:47:34 +0900

    chore: 유의사항 수정

--- src/components/canvas/canvas-bottom-text.tsx
+++ src/components/canvas/canvas-bottom-text.tsx
@@ -28,37 +28,41 @@ export default function CanvasBottomText({
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-4">
-        <ul>
-          <p className="font-bold">유의사항</p>
-          <li>- GPT의 답변은 오직 영어로만 가능합니다</li>
-          <li>- 로그인해야만 GPT,업로드 기능을 이용할 수 있습니다.</li>
-          <li>- 추후 계정 당 하루 이용 횟수가 제한될 수 있습니다.</li>
-        </ul>
+        <div className="flex flex-col">
+          <p className="font-bold text-lg">유의사항</p>
+          <ul className="flex flex-col">
+            <li>- GPT의 답변은 오직 영어로만 가능합니다</li>
+            <li>
+              - 마우스 좌클릭으로 얇은 선, 우클릭으로 굵은 선을 그릴 수
+              있습니다.
+            </li>
+            <li>- 로그인해야만 GPT와 업로드 기능을 이용할 수 있습니다.</li>
+            <li>- 추후 계정 당 하루 이용 횟수가 제한될 수 있습니다.</li>
+          </ul>
+        </div>
 
-        {session && (
-          <div className="flex items-center gap-2">
-            <button
-              onClick={sendImage}
-              disabled={isMessageLoading || !session}
-              className="flex items-center gap-2 px-4 py-2 bg-purple-500 disabled:opacity-50 text-white rounded-md transition-colors"
-            >
-              <Image
-                src={"./gpt-logo.svg"}
-                alt="gpt-logo"
-                width={20}
-                height={20}
-              />
-              GPT에 전송
-            </button>
-            <button
-              onClick={handleSendPost}
-              disabled={isSending}
-              className=" bg-red-300 text-white px-4 py-2 rounded-md"
-            >
-              {isSending ? "업로드 중..." : "박물관에 업로드하기"}
-            </button>
-          </div>
-        )}
+        <div className="flex items-center gap-2">
+          <button
+            onClick={sendImage}
+            disabled={isMessageLoading || !session}
+            className="flex items-center gap-2 px-4 py-2 bg-purple-500 disabled:opacity-50 text-white rounded-md transition-colors"
+          >
+            <Image
+              src={"./gpt-logo.svg"}
+              alt="gpt-logo"
+              width={20}
+              height={20}
+            />
+            GPT에 전송
+          </button>
+          <button
+            onClick={handleSendPost}
+            disabled={isSending || !session}
+            className=" bg-red-300 text-white px-4 py-2 rounded-md disabled:opacity-50"
+          >
+            {isSending ? "업로드 중..." : "박물관에 업로드하기"}
+          </button>
+        </div>
       </div>
       <GptAnswer isMessageLoading={isMessageLoading} message={message} />
       <ToastContainer
